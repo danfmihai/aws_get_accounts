97e4d536edccd9ef82a86becea0b494fb0810777
Mihai <danfmihai@hotmail.com>
2021-12-16 20:42:50 +0000

    used fmt

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.69.0"
     }
   }
 }
 
 locals {
-  home_dir     = pathexpand("~/")
+  home_dir = pathexpand("~/")
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region                  = "us-east-1"
   shared_credentials_file = "${local.home_dir}/.aws/credentials"
   profile                 = "mihai"
 }
