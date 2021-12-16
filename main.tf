6b31db585eef982bf3d8a377985910ef653ee6b4
Mihai <danfmihai@hotmail.com>
2021-12-16 20:42:36 +0000

    add read users

--- main.tf
+++ main.tf
@@ -1,1 +1,4 @@
 data "aws_organizations_organization" "myorg" {}
+
+data "aws_iam_users" "users" {
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "account_ids" {
   value = data.aws_organizations_organization.myorg.accounts[*].id
 }
+
+output "users" {
+  value = data.aws_iam_users.users.names
+}
